67af480f5246ec8cad0b268d96a3061cf4381d83
ANEOA <neo.anonim.16@gmail.com>
2024-09-29 16:48:14 +0300

    update class TelegramBotApi()

--- app/Services/Telegram/TelegramBotApi.php
+++ app/Services/Telegram/TelegramBotApi.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Facades\Http;
 class TelegramBotApi
 {
     public const HOST = 'https://api.telegram.org/bot';
-    public static function sendMessage($token, $chat_id, $text)
+    public static function sendMessage($token, $chat_id, $message)
     {
         Http::get(self::HOST . $token . '/sendMessage', [
             'chat_id' => $chat_id,
-            'text' => $text,
+            'text' => $message,
         ]);
     }
 }
